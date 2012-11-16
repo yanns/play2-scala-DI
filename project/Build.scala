568c4593b9b58da0d216c3fa2b6a9b6b91216af9
Yann Simon <yann.simon.fr@gmail.com>
2012-11-16 10:29:38 +0100

    update to last play 2.1 version

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object ApplicationBuild extends Build {
       "org.springframework" % "spring-context" % "3.0.7.RELEASE"
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(
       // Add your own project settings here
     )
 
